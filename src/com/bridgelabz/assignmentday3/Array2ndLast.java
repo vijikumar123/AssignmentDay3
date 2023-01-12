0cf476f6e6e0a3b89d85d9d872b151440d51f1b4
vijikumar123 <vk9164940756@gmail.com>
2023-01-12 22:13:06 +0530

    added: assignmentday3

---
+++ src/com/bridgelabz/assignmentday3/Array2ndLast.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.assignmentday3;
+import java.util.Arrays;
+public class Array2ndLast {
+    public static int getSecondLargest(int[] a, int total){
+        Arrays.sort(a);
+        return a[total-2];
+    }
+    public static void main(String args[]){
+        int a[]={1,2,5,6,3,2};
+        int b[]={44,66,99,77,33,22,55};
+        System.out.println("Second Largest: "+getSecondLargest(a,6));
+        System.out.println("Second Largest: "+getSecondLargest(b,7));
+    }
+}
